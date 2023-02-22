e1d56290a60663b1d4c3f83c23c15807a1774594
Abdelsalam <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2023-02-22 21:55:55 +0200

    ReportResponse

---
+++ src/main/java/me/readyplayer/reportingservice/ReportResponse.java
@@ -0,0 +1,10 @@
+package me.readyplayer.reportingservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ReportResponse {
+    private String reportLink;
+}
