76fcc51376aef5ada74725b5dcd9afd8b4ce7622
Abdelsalam <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2023-02-22 22:01:33 +0200

    exception handler

---
+++ src/main/java/me/readyplayer/reportingservice/exceptions/CustomException.java
@@ -0,0 +1,7 @@
+package me.readyplayer.reportingservice.exceptions;
+
+public class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/me/readyplayer/reportingservice/exceptions/ExceptionHandler.java
@@ -0,0 +1,46 @@
+package me.readyplayer.reportingservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @org.springframework.web.bind.annotation.ExceptionHandler(CustomException.class)
+    public Map<String, String> handleCustomException(CustomException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("error", exception.getMessage());
+
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpMessageNotReadableException.class)
+    public Map<String, String> handleMessageNotReadableException(HttpMessageNotReadableException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("error", exception.getMessage());
+
+        return errorMap;
+    }
+}
